f035cbaf1441a5d7b194b93f63b93227484917da
herbanarhitech <jeff.badette@protonmail.com>
2021-09-23 03:31:17 -0400

    corrected syntax error

---
+++ fahrenheit_to_celsius/FahrenheitToCelsius.java
@@ -0,0 +1,20 @@
+//Rudimentary program created to understand semantics and syntax of Java
+
+
+
+public class FahrenheitToCelsius{
+		public static void main(String[] args) {
+
+			int saturdayFahrenheit = 78, sundayFahrenheit = 81;
+
+			System.out.println(saturdayFahrenheit);
+			System.out.print(sundayFahrenheit);
+
+
+
+
+		}
+
+}
+			
+
---
+++ fahrenheit_to_celsius/fahrenheit_to_celsius.java
@@ -0,0 +1,15 @@
+//Rudimentary program created to understand semantics and syntax of Java
+
+
+
+public class fahrenheit_to_celsius{
+		public static void main(String[] args) {
+			int saturdayFahrenheit= 78,  sundayFahrenheit=81;
+
+			System.out.printl("Temperature for Saturay is\n, and the temperature for Sunday is\n",saturdayFahrenheit, sundayFahrenheit);
+
+		}
+
+}
+			
+
