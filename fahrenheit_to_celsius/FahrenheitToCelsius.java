a6cceb1ea424a311b2a8da3c3f199530ac4447f1
herbanarhitech <jeff.badette@protonmail.com>
2021-09-23 21:44:56 -0400

    got algorith got work

--- fahrenheit_to_celsius/FahrenheitToCelsius.java
+++ fahrenheit_to_celsius/FahrenheitToCelsius.java
@@ -1,14 +1,31 @@
 //Rudimentary program created to understand semantics and syntax of Java
 
-
+/* temperature formulas
+ * C = 5/9 x (F-32)
+ * F= (9/5*C) + 32
+ *
+ * reminder 9/5 =1.8 5/9 = .55 */
 
 public class FahrenheitToCelsius{
 		public static void main(String[] args) {
 
 			int saturdayFahrenheit = 78, sundayFahrenheit = 81;
 
-			System.out.println(saturdayFahrenheit);
-			System.out.print(sundayFahrenheit);
+			double saturdayCelsius = (5.0/9) * (saturdayFahrenheit -32), sundayCelsius= (5.0/9) * (sundayFahrenheit -32);
+
+			//System.out.println(saturdayFahrenheit); // used to for testing purposes 
+
+			//System.out.print(sundayFahrenheit); // used to testing purposes
+			
+			System.out.print("These will be your weekend averages");
+
+			System.out.print("For Saturdays weahther:" + saturdayCelsius);
+
+			System.out.print("For Sundays weather:" + sundayCelsius);
+
+
+			
+
 
 
 
